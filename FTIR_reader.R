af2bb2366d6294a435778d458282bbb33a0d3fbd
Jonah Gezelter <jonah@dhcp-10-250-140-44.harvard.edu>
2024-07-01 17:54:29 -0400

    updates to ordering

--- FTIR_reader.R
+++ FTIR_reader.R
@@ -3,7 +3,8 @@ library(tidyverse)
 read_FTIR <- function(path){
   out <- read_csv(path, 
                   col_names = F, 
-                  id = "path") %>% 
+                  id = "path", 
+                  show_col_types = F) %>% 
     mutate(wavelength_nm = (1 / X1) * (10 ^ 7), 
            value = X2, 
            date = basename(dirname(path)), 
--- UV_Vis_reader.R
+++ UV_Vis_reader.R
@@ -2,12 +2,15 @@ library(tidyverse)
 library(janitor)
 
 read_uv_vis <- function(file){
-  data <- read_csv(file, col_names = T, skip = 1) %>% 
+  data <- read_csv(file, 
+                   col_names = T, 
+                   skip = 1, 
+                   show_col_types = F) %>% 
     clean_names()
   
   path <- file
   
-  headers <- read_csv(file, col_names = T) %>% slice(c(1))
+  headers <- read_csv(file, col_names = T, show_col_types = F) %>% slice(c(1))
   
   cols <- ncol(data)
   rows <- nrow(data)
@@ -30,11 +33,43 @@ read_uv_vis <- function(file){
                into = c("solvent", "dye", "concentration", "measurement_num"), 
                sep = "_", 
                remove = F) %>% 
-      mutate(mode = str_extract(mode, ".+(?=\\.csv)"))
+      mutate(mode = str_extract(mode, ".+(?=\\.csv)"), 
+             concentration_value = as.numeric(str_extract(concentration, "[^[:alpha:]]+")), 
+             concentration_type = str_extract(concentration, "[:alpha:]+"))
     if(!str_detect(sample, "Baseline")){
       out <- out %>% rbind(data_cur)
     }
     
   }
-  return(out)
+  
+  out_help <- out %>% 
+    filter(dye == "")
+  
+  out_help_3 <- out %>% 
+    filter(dye != "")
+  
+  out_help_2 <- unique(out_help_3$dye)
+  
+  out2 <- data.frame()
+  
+  for(i in 1:length(out_help_2)){
+    out_help_4 <- out_help %>% 
+      mutate(dye = out_help_2[i])
+    
+    out2 <- out2 %>% rbind(out_help_4)
+  }
+  
+  out2 <- out2 %>% 
+    mutate(dye = as.character(dye)) %>%  
+    bind_rows(out_help_3)
+  
+  conc_orderer <- out2 %>% 
+    count(concentration_value, concentration_type) %>% 
+    mutate(conc_order = str_c(concentration_value, concentration_type))
+  
+  out2 <- out2 %>% 
+    mutate(concentration = factor(concentration, 
+                                  levels = conc_orderer$conc_order))
+  
+  return(out2)
 }
