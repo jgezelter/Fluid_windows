1fd10809c7ad0750bb4aef8db50d27d1da3ddb13
Jonah Gezelter <jonah@dhcp-10-250-140-44.harvard.edu>
2024-07-11 15:47:30 -0400

    film plotter made, updating for megan

--- FTIR_reader.R
+++ FTIR_reader.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-read_FTIR <- function(path){
+read_FTIR_IS_fluid <- function(path){
   out <- read_csv(path, 
                   col_names = F, 
                   id = "path", 
@@ -20,6 +20,27 @@ read_FTIR <- function(path){
                       "measurement_num"), 
              sep = "_") %>% 
     mutate(measurement_num = str_extract(measurement_num, "[:alnum:]+"))
+  
+  return(out)
+}
+
+read_FTIR_IS_film <- function(path){
+  out <- read_csv(path, 
+                  col_names = F, 
+                  id = "path", 
+                  show_col_types = F) %>% 
+    mutate(wavelength_nm = (1 / X1) * (10 ^ 7), 
+           value = X2, 
+           date = basename(dirname(path)), 
+           meta = basename(path),
+           .keep = "none") %>% 
+    separate(meta, 
+             into = c("film",
+                      "power", 
+                      "time", 
+                      "attachment", 
+                      "mode"), 
+             sep = "_") 
   
   return(out)
 }
--- UV_Vis_reader.R
+++ UV_Vis_reader.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(janitor)
 
-read_uv_vis <- function(file){
+read_uv_vis_is_fluid <- function(file){
   data <- read_csv(file, 
                    col_names = T, 
                    skip = 1, 
@@ -42,34 +42,217 @@ read_uv_vis <- function(file){
     
   }
   
-  out_help <- out %>% 
-    filter(dye == "")
+  # out_help <- out %>% 
+  #   filter(dye == "")
+  # 
+  # out_help_3 <- out %>% 
+  #   filter(dye != "")
+  # 
+  # out_help_2 <- unique(out_help_3$dye)
+  # 
+  # out2 <- data.frame()
+  # 
+  # for(i in 1:length(out_help_2)){
+  #   out_help_4 <- out_help %>% 
+  #     mutate(dye = out_help_2[i])
+  #   
+  #   out2 <- out2 %>% rbind(out_help_4)
+  # }
+  # 
+  # out2 <- out %>% 
+  #   mutate(dye = as.character(dye)) %>%  
+  #   bind_rows(out_help_3)
   
-  out_help_3 <- out %>% 
-    filter(dye != "")
-  
-  out_help_2 <- unique(out_help_3$dye)
-  
-  out2 <- data.frame()
-  
-  for(i in 1:length(out_help_2)){
-    out_help_4 <- out_help %>% 
-      mutate(dye = out_help_2[i])
-    
-    out2 <- out2 %>% rbind(out_help_4)
-  }
-  
-  out2 <- out2 %>% 
-    mutate(dye = as.character(dye)) %>%  
-    bind_rows(out_help_3)
-  
-  conc_orderer <- out2 %>% 
+  conc_orderer <- out %>% 
     count(concentration_value, concentration_type) %>% 
     mutate(conc_order = str_c(concentration_value, concentration_type))
   
-  out2 <- out2 %>% 
+  out2 <- out %>% 
     mutate(concentration = factor(concentration, 
                                   levels = conc_orderer$conc_order))
   
   return(out2)
+}
+
+
+read_uv_vis_uma_fluid <- function(file){
+  data <- read_csv(file, 
+                   col_names = T, 
+                   skip = 1, 
+                   show_col_types = F) %>% 
+    clean_names()
+  
+  path <- file
+  
+  headers <- read_csv(file, col_names = T, show_col_types = F) %>% slice(c(1))
+  
+  cols <- ncol(data)
+  rows <- nrow(data)
+  out <- data.frame()
+  
+  for(i in 1:(cols / 2)){
+    sample <- names(headers)[[(2 * i) - 1]]
+    
+    data_cur <- data[,((2 * i) - 1):((2 * i))] 
+    data_cur <- data_cur %>% 
+      mutate(measurement_type = str_extract(names(data_cur)[2], ".+(?=[:digit:])")) %>% 
+      rename_with(.fn = function(x){case_when(str_detect(x, "wavelength_nm") ~ "wavelength_nm", 
+                                              str_detect(x, "measurement_type") ~ "measurement_type",
+                                              .default = "value")}) %>% 
+      mutate(sample = sample, 
+             id = basename(path), 
+             date = basename(dirname(path))) %>% 
+      separate(id, into = c("machine", "attachment", "mode"), sep = "_") %>% 
+      separate(sample, 
+               into = c("solvent", "dye", "concentration", "measurement_num", "detector_angle", "sample_angle"), 
+               sep = "_", 
+               remove = F) %>% 
+      mutate(mode = str_extract(mode, ".+(?=\\.csv)"), 
+             concentration_value = as.numeric(str_extract(concentration, "[^[:alpha:]]+")), 
+             concentration_type = str_extract(concentration, "[:alpha:]+"))
+    if(!str_detect(sample, "Baseline")){
+      out <- out %>% rbind(data_cur)
+    }
+    
+  }
+  
+  # out_help <- out %>% 
+  #   filter(dye == "")
+  # 
+  # out_help_3 <- out %>% 
+  #   filter(dye != "")
+  # 
+  # out_help_2 <- unique(out_help_3$dye)
+  # 
+  # out2 <- data.frame()
+  # 
+  # for(i in 1:length(out_help_2)){
+  #   out_help_4 <- out_help %>% 
+  #     mutate(dye = out_help_2[i])
+  #   
+  #   out2 <- out2 %>% rbind(out_help_4)
+  # }
+  # 
+  # out2 <- out %>% 
+  #   mutate(dye = as.character(dye)) %>%  
+  #   bind_rows(out_help_3)
+  
+  conc_orderer <- out %>% 
+    count(concentration_value, concentration_type) %>% 
+    mutate(conc_order = str_c(concentration_value, concentration_type))
+  
+  out2 <- out %>% 
+    mutate(concentration = factor(concentration, 
+                                  levels = conc_orderer$conc_order))
+  
+  return(out2)
+}
+
+read_uv_vis_is_film <- function(file){
+  data <- read_csv(file, 
+                   col_names = T, 
+                   skip = 1, 
+                   show_col_types = F) %>% 
+    clean_names()
+  
+  path <- file
+  
+  headers <- read_csv(file, col_names = T, show_col_types = F) %>% slice(c(1))
+  
+  cols <- ncol(data)
+  rows <- nrow(data)
+  out <- data.frame()
+  
+  for(i in 1:(cols / 2)){
+    sample <- names(headers)[[(2 * i) - 1]]
+    
+    data_cur <- data[,((2 * i) - 1):((2 * i))] 
+    data_cur <- data_cur %>% 
+      mutate(measurement_type = str_extract(names(data_cur)[2], ".+(?=[:digit:])")) %>% 
+      rename_with(.fn = function(x){case_when(str_detect(x, "wavelength_nm") ~ "wavelength_nm", 
+                                              str_detect(x, "measurement_type") ~ "measurement_type",
+                                              .default = "value")}) %>% 
+      mutate(sample = sample, 
+             id = basename(path), 
+             date = basename(dirname(path))) %>% 
+      separate(id, into = c(NA, NA, NA, NA, "mode"), sep = "_") %>% 
+      separate(sample, 
+               into = c("film", "power", "time", NA, "mode"), 
+               sep = "_", 
+               remove = F) 
+    if(!str_detect(sample, "Baseline")){
+      out <- out %>% rbind(data_cur)
+    }
+    
+  }
+  
+  # out_help <- out %>% 
+  #   filter(dye == "")
+  # 
+  # out_help_3 <- out %>% 
+  #   filter(dye != "")
+  # 
+  # out_help_2 <- unique(out_help_3$dye)
+  # 
+  # out2 <- data.frame()
+  # 
+  # for(i in 1:length(out_help_2)){
+  #   out_help_4 <- out_help %>% 
+  #     mutate(dye = out_help_2[i])
+  #   
+  #   out2 <- out2 %>% rbind(out_help_4)
+  # }
+  # 
+  # out2 <- out %>% 
+  #   mutate(dye = as.character(dye)) %>%  
+  #   bind_rows(out_help_3)
+  
+  return(out)
+}
+
+
+read_uv_vis_uma_film <- function(file){
+  data <- read_csv(file, 
+                   col_names = T, 
+                   skip = 1, 
+                   show_col_types = F) %>% 
+    clean_names()
+  
+  path <- file
+  
+  headers <- read_csv(file, col_names = T, show_col_types = F) %>% slice(c(1))
+  
+  cols <- ncol(data)
+  rows <- nrow(data)
+  out <- data.frame()
+  
+  for(i in 1:(cols / 2)){
+    sample <- names(headers)[[(2 * i) - 1]]
+    
+    data_cur <- data[,((2 * i) - 1):((2 * i))] 
+    data_cur <- data_cur %>% 
+      mutate(measurement_type = str_extract(names(data_cur)[2], ".+(?=[:digit:])")) %>% 
+      rename_with(.fn = function(x){case_when(str_detect(x, "wavelength_nm") ~ "wavelength_nm", 
+                                              str_detect(x, "measurement_type") ~ "measurement_type",
+                                              .default = "value")}) %>% 
+      mutate(sample = sample, 
+             id = basename(path), 
+             date = basename(dirname(path))) %>% 
+      separate(id, into = c("film", "power", "time", NA), sep = "_") %>% 
+      separate(sample, 
+               into = c(NA, "angle_of_incidence", "detector_angle"), 
+               sep = " ", 
+               remove = F) %>% 
+      mutate(angle_of_incidence = as.numeric(angle_of_incidence), 
+             detector_angle = as.numeric(detector_angle))
+    
+    if(!str_detect(sample, "Baseline")){
+      out <- out %>% rbind(data_cur)
+    }
+    
+  }
+  
+ 
+  
+  return(out)
 }
