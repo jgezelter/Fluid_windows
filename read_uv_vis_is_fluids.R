c23244857158570c7c6744761bbbf19540059dc2
Jonah Gezelter <jonah@Jonahs-web-traversing-device-3.local>
2024-09-16 09:09:41 -0400

    made read_uv_vis_is_fluids more generic so it can be used to bulk process any data from UV-Vis IS, even not from this project

--- read_uv_vis_is_fluids.R
+++ read_uv_vis_is_fluids.R
@@ -1,4 +1,5 @@
 read_uv_vis_is_fluid <- function(path){
+  #reads the data and makes a dataframe from it, with numbered column headers
   data <- read_csv(path, 
                    col_names = T, 
                    skip = 1,
@@ -6,17 +7,22 @@ read_uv_vis_is_fluid <- function(path){
                    name_repair = "unique_quiet") %>% 
     clean_names()
   
-  
+  #reads the data and makes a dataframe containing only the column headers
   headers <- read_csv(path, 
                       col_names = T, 
                       show_col_types = F, 
                       name_repair = "unique_quiet") %>% 
     slice(c(1))
   
+  #some helper data for later
   cols <- ncol(data)
   rows <- nrow(data)
   out <- data.frame()
   
+  #grabs pairs of columns from the data dataframe and the matching headers from the headers dataframe
+  #pastes them together
+  #then "lengthens" the data by pasting the pairs of columns top to bottom 
+  #includes wavelength, intensity data, data path, and sample name
   for(i in 1:(cols / 2)){
     sample <- names(headers)[[(2 * i) - 1]]
     
@@ -25,13 +31,7 @@ read_uv_vis_is_fluid <- function(path){
       rename_with(.fn = function(x){case_when(str_detect(x, "wavelength_nm") ~ "wavelength_nm", 
                                               .default = "value")}) %>% 
       mutate(sample_id = sample, 
-             scan_info = str_extract(basename(path), ".+(?=\\.)"),
-             date = basename(dirname(path))) %>% 
-      separate(scan_info, into = c("machine", "attachment", "mode"), sep = "_") %>% 
-      mutate(mode = case_when(mode == "C" ~ str_c("C-", str_extract(sample_id, "(?<=-)[:digit:]+")), 
-                              .default = mode), 
-             sample_id = case_when(str_detect(sample_id, "-") ~ str_extract(sample_id, ".+(?=-)"), 
-                                   .default = sample_id)) %>% 
+             path = path) %>% 
       filter(!str_detect(wavelength_nm, "[^[:digit:]]")) %>% 
       mutate(wavelength_nm = as.numeric(wavelength_nm), 
              value = as.numeric(value))
